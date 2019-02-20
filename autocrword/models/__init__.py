2db1b6fbf9059382a80b59d241e8fd490c933a4c
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-02-20 16:39:28 +0800

    init 1.0

--- autocrword/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import autoproject, autowind, autocivil, autoeconomic, autoelectrical
--- autocrword/models/test.py
+++
@@ -1,3 +0,0 @@
-from odoo import models, fields, api
-import renew2
-renew2.read_excel()
