bc60ad8138b84d07adeff8194b6881a74a3e32a4
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-02-21 15:32:54 +0800

    base finished chapter 6 20%

--- autocrword/models/chapter_6/calculate.py
+++ autocrword/models/chapter_6/calculate.py
@@ -1,8 +1,8 @@
-import decimal
-from decimal import *
-
-Context(prec=4, rounding=ROUND_HALF_EVEN, Emin=-999999, Emax=999999, capitals=1, clamp=0, flags=[],
-        traps=[InvalidOperation, DivisionByZero, Overflow])
+# import decimal
+# from decimal import *
+#
+# Context(prec=4, rounding=ROUND_HALF_EVEN, Emin=-999999, Emax=999999, capitals=1, clamp=0, flags=[],
+#         traps=[InvalidOperation, DivisionByZero, Overflow])
 
 
 class ElectricalCircuit:
@@ -33,74 +33,87 @@ class WireRod(ElectricalCircuit):
 
 
 class TowerType(ElectricalCircuit):
-    def __init__(self):
-        super().__init__(25.3, 23.6, 1.55, 3, 31, 5, )
+    def __init__(self, *value_list):
+        ElectricalCircuit.__init__(self, *value_list)
         self.tower_type = ''
         self.tower_type_high = ''
         self.used_numbers = ''
         self.tower_weight = 0
         self.tower_number_weight = 0
+        self.tower_height = 0
+        self.tower_foot_distance = 0
 
+        self.tower_area = 0
+        self.sum_tower_area = 0
+        self.tower_number_area = []
         self.sum_used_numbers = 0
-        # self.sum_tower_weight = 0
         self.sum_tower_number_weight = 0
-        #
-        # self.tower_type_list = tower_type_list
-        # self.tower_type_high_list = tower_type_high_list
-        # self.tower_weight_list = tower_weight_list
+        self.kilometer_tower_number = 0
 
-    def tower_type_models(self, tower_type, tower_type_high, tower_weight):
+    def tower_type_models(self, tower_type, tower_type_high, tower_weight, tower_height, tower_foot_distance):
         self.tower_type = tower_type
         self.tower_type_high = tower_type_high
         self.tower_weight = tower_weight
+        self.tower_height = tower_height
+        self.tower_foot_distance = tower_foot_distance
 
         if self.tower_type == "单回耐张塔":
             if self.tower_type_high == "J2-24" or self.tower_type_high == "J4-24":
                 self.used_numbers = round(self.single_circuit / 3, 0)
                 self.tower_number_weight = self.tower_weight * self.used_numbers
-
+                self.tower_area = self.used_numbers * self.tower_foot_distance ** 2
             elif self.tower_type_high == "FS-18":
                 self.used_numbers = round(((self.line_loop_number * 100 / 2) / 100 + 1), 0)
                 self.tower_number_weight = self.tower_weight * self.used_numbers
+                self.tower_area = self.used_numbers * self.tower_foot_distance ** 2
 
         if self.tower_type == "单回直线塔":
             if self.tower_type_high == "Z2-30" or self.tower_type_high == "ZK-42":
                 self.used_numbers = round(self.single_circuit * 1000 / 250 / 2, 0)
-                self.tower_number_weight =self.tower_weight * self.used_numbers
+                self.tower_number_weight = self.tower_weight * self.used_numbers
+                self.tower_area = self.used_numbers * self.tower_foot_distance ** 2
 
         if self.tower_type == "双回耐张塔":
             if self.tower_type_high == "SJ2-24" or self.tower_type_high == "SJ4-24":
                 self.used_numbers = round(self.double_circuit / 3, 0)
                 self.tower_number_weight = self.tower_weight * self.used_numbers
+                self.tower_area = self.used_numbers * self.tower_foot_distance ** 2
 
         if self.tower_type == "双回直线塔":
             if self.tower_type_high == "SZ2-30" or self.tower_type_high == "SZK-42":
                 self.used_numbers = round(self.double_circuit * 1000 / 220 / 2, 0)
                 self.tower_number_weight = self.tower_weight * self.used_numbers
+                self.tower_area = self.used_numbers * self.tower_foot_distance ** 2
 
         if self.tower_type == "铁塔电缆支架":
             if self.tower_type_high == "角钢":
                 self.used_numbers = self.tur_number
                 self.tower_number_weight = self.tower_weight * self.used_numbers
+                self.tower_area = 0
 
-    def sum_cal(self, tower_type_list, tower_type_high_list, tower_weight_list):
+    def sum_cal(self, tower_type_list, tower_type_high_list, tower_weight_list, tower_height_list,
+                tower_foot_distance_list):
         for i in range(0, len(tower_weight_list)):
-            TowerType.tower_type_models(self, tower_type_list[i], tower_type_high_list[i], tower_weight_list[i])
+            TowerType.tower_type_models(self, tower_type_list[i], tower_type_high_list[i], tower_weight_list[i],
+                                        tower_height_list[i], tower_foot_distance_list[i])
             print(self.tower_type, self.tower_type_high, self.used_numbers)
-               # self.sum_tower_weight += self.tower_number_weight
-            if self.tower_type=="铁塔电缆支架":
+            if self.tower_type == "铁塔电缆支架":
                 self.sum_used_numbers = self.sum_used_numbers
             else:
                 self.sum_used_numbers = int(self.used_numbers) + self.sum_used_numbers
             self.sum_tower_number_weight = float(self.tower_number_weight) + self.sum_tower_number_weight
+            self.tower_number_area.append(self.tower_area)
+            self.kilometer_tower_number = self.sum_used_numbers / (self.single_circuit + self.double_circuit)
+            self.sum_tower_area = float(self.tower_area) + self.sum_tower_area
 
 
 # project01 = ElectricalCircuit(25.3, 23.6, 1.55, 3, 31, 5)
-project02 = TowerType()
+project02 = TowerType(25.3, 23.6, 1.55, 3, 31, 5)
 tower_type_list = ['单回耐张塔', '单回耐张塔', '单回耐张塔', '单回直线塔', '单回直线塔', '双回耐张塔', '双回耐张塔', '双回直线塔', '双回直线塔', '铁塔电缆支架']
 tower_type_high_list = ['J2-24', 'J4-24', 'FS-18', 'Z2-30', 'ZK-42', 'SJ2-24', 'SJ4-24', 'SZ2-30', 'SZK-42', '角钢']
 tower_weight_list = [6.8, 8.5, 7, 5.5, 8.5, 12.5, 17, 6.5, 10, 0.5, ]
+tower_height_list = [32, 32, 27, 37, 49, 37, 37, 42, 54, 0]
+tower_foot_distance_list = [5.5, 5.5, 6, 5, 6, 7, 8, 6, 8, 0]
+project02.sum_cal(tower_type_list, tower_type_high_list, tower_weight_list, tower_height_list, tower_foot_distance_list)
 
-project02.sum_cal(tower_type_list, tower_type_high_list, tower_weight_list)
-
-print(project02.sum_tower_number_weight)
+print(project02.sum_tower_area)
