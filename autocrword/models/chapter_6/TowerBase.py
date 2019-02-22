e69a30fa9030be709d7328c502e99cdc2d9d3345
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-02-22 09:38:12 +0800

    base finished chapter 6 60%

--- autocrword/models/chapter_6/TowerBase.py
+++ autocrword/models/chapter_6/TowerBase.py
@@ -1,51 +1,51 @@
 from TowerType import TowerType
-from roundUp import roundUp
+from RoundUp import round_up
 
 
 class TowerBase(TowerType):
     def __init__(self, *value_list):
         TowerType.__init__(self, *value_list)
 
-        self.used_numbers_base_ZJC1 = ''
-        self.used_numbers_base_ZJC2 = ''
-        self.used_numbers_base_JJC1 = ''
-        self.used_numbers_base_JJC2 = ''
-        self.used_numbers_base_TW1 = 0
-        self.used_numbers_base_TW2 = 0
+        self.used_numbers_base_zjc1 = ''
+        self.used_numbers_base_zjc2 = ''
+        self.used_numbers_base_jjc1 = ''
+        self.used_numbers_base_jjc2 = ''
+        self.used_numbers_base_tw1 = 0
+        self.used_numbers_base_tw2 = 0
         self.used_numbers_base_layer = 0
 
-    def electrical_insulator_model(self, base_ZJC1, base_ZJC2, base_JJC1, base_JJC2, base_TW1, base_TW2, base_layer):
-        self.used_numbers_base_ZJC1 = base_ZJC1
-        self.used_numbers_base_ZJC2 = base_ZJC2
-        self.used_numbers_base_JJC1 = base_JJC1
-        self.used_numbers_base_JJC2 = base_JJC2
-        self.used_numbers_base_TW1 = base_TW1
-        self.used_numbers_base_TW2 = base_TW2
+    def electrical_insulator_model(self, base_zjc1, base_zjc2, base_jjc1, base_jjc2, base_tw1, base_tw2, base_layer):
+        self.used_numbers_base_zjc1 = base_zjc1
+        self.used_numbers_base_zjc2 = base_zjc2
+        self.used_numbers_base_jjc1 = base_jjc1
+        self.used_numbers_base_jjc2 = base_jjc2
+        self.used_numbers_base_tw1 = base_tw1
+        self.used_numbers_base_tw2 = base_tw2
         self.used_numbers_base_layer = base_layer
 
-        if self.used_numbers_base_ZJC1 == "ZJC1":
+        if self.used_numbers_base_zjc1 == "ZJC1":
 
-            self.used_numbers_base_ZJC1 = roundUp(
+            self.used_numbers_base_zjc1 = round_up(
                 (self.used_numbers_single_Z2_30 + self.used_numbers_double_SZ2_30) / 2, 0)
 
-        if self.used_numbers_base_ZJC2 == "ZJC2":
-            self.used_numbers_base_ZJC2 = roundUp(
+        if self.used_numbers_base_zjc2 == "ZJC2":
+            self.used_numbers_base_zjc2 = round_up(
                 (self.used_numbers_single_ZK_42 + self.used_numbers_double_SZK_42) / 2, 0)
 
-        if self.used_numbers_base_JJC1 == "JJC1":
-            self.used_numbers_base_JJC1 = self.used_numbers_single_J2_24 + self.used_numbers_double_SJ2_24
+        if self.used_numbers_base_jjc1 == "JJC1":
+            self.used_numbers_base_jjc1 = self.used_numbers_single_J2_24 + self.used_numbers_double_SJ2_24
 
-        if self.used_numbers_base_JJC2 == "JJC2":
-            self.used_numbers_base_JJC2 = self.used_numbers_single_J4_24 + self.used_numbers_single_FS_18 \
+        if self.used_numbers_base_jjc2 == "JJC2":
+            self.used_numbers_base_jjc2 = self.used_numbers_single_J4_24 + self.used_numbers_single_FS_18 \
                                           + self.used_numbers_double_SJ4_24
 
-        if self.used_numbers_base_TW1 == "TW1":
-            self.used_numbers_base_TW1 = self.used_numbers_single_Z2_30 + self.used_numbers_double_SZ2_30 \
-                                         - self.used_numbers_base_ZJC1
+        if self.used_numbers_base_tw1 == "TW1":
+            self.used_numbers_base_tw1 = self.used_numbers_single_Z2_30 + self.used_numbers_double_SZ2_30 \
+                                         - self.used_numbers_base_zjc1
 
-        if self.used_numbers_base_TW2 == "TW2":
-            self.used_numbers_base_TW2 = self.used_numbers_single_ZK_42 + self.used_numbers_double_SZK_42 \
-                                         - self.used_numbers_base_ZJC2
+        if self.used_numbers_base_tw2 == "TW2":
+            self.used_numbers_base_tw2 = self.used_numbers_single_ZK_42 + self.used_numbers_double_SZK_42 \
+                                         - self.used_numbers_base_zjc2
 
         if self.used_numbers_base_layer == "基础垫层":
             self.used_numbers_base_layer = self.sum_used_numbers
@@ -61,6 +61,6 @@ tower_foot_distance_list = [5.5, 5.5, 6, 5, 6, 7, 8, 6, 8, 0]
 project02 = TowerBase(25.3, 23.6, 1.55, 3, 31, 5)
 project02.sum_cal(tower_type_list, tower_type_high_list, tower_weight_list, tower_height_list, tower_foot_distance_list)
 project02.electrical_insulator_model(*tower_base_list)
-print(project02.used_numbers_base_ZJC1, project02.used_numbers_base_ZJC2, project02.used_numbers_base_JJC1,
-      project02.used_numbers_base_JJC2, project02.used_numbers_base_TW1, project02.used_numbers_base_TW2,
+print(project02.used_numbers_base_zjc1, project02.used_numbers_base_zjc2, project02.used_numbers_base_jjc1,
+      project02.used_numbers_base_jjc2, project02.used_numbers_base_tw1, project02.used_numbers_base_tw2,
       project02.used_numbers_base_layer)
