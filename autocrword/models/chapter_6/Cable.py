f2f856e6fb4f7f59194e9feeb439cdd13584309e
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-02-25 09:47:08 +0800

    base finished chapter 6 60%

--- autocrword/models/chapter_6/Cable.py
+++ autocrword/models/chapter_6/Cable.py
@@ -1,155 +1,55 @@
 from ElectricalCircuit import ElectricalCircuit
-from RoundUp import round_up
 
 
 class Cable(ElectricalCircuit):
     """
     电缆
     """
+
     def __init__(self, *value_list):
-        TowerType.__init__(self, *value_list)
-        self.used_numbers_base_type = ''
-        self.c25_unit_list = []
-        self.c25_unit_zjc1, self.c25_unit_zjc2, self.c25_unit_jjc1 = 0, 0, 0
-        self.c25_unit_jjc2, self.c25_unit_tw1, self.c25_unit_tw2 = 0, 0, 0
-        self.c25_unit_layer, self.c25_unit = 0, 0
-        self.c25_sum_zjc1, self.c25_sum_zjc2, self.c25_sum_jjc1 = 0, 0, 0
-        self.c25_sum_jjc2, self.c25_sum_tw1, self.c25_sum_tw2 = 0, 0, 0
-        self.c25_sum_layer, self.c25_sum = 0, 0
+        ElectricalCircuit.__init__(self, *value_list)
 
-        self.steel_unit_list = []
-        self.steel_unit_zjc1, self.steel_unit_zjc2, self.steel_unit_jjc1 = 0, 0, 0
-        self.steel_unit_jjc2, self.steel_unit_tw1, self.steel_unit_tw2 = 0, 0, 0
-        self.steel_unit_layer, self.steel_unit = 0, 0
-        self.steel_sum_zjc1, self.steel_sum_zjc2, self.steel_sum_jjc1 = 0, 0, 0
-        self.steel_sum_jjc2, self.steel_sum_tw1, self.steel_sum_tw2 = 0, 0, 0
-        self.steel_sum_layer, self.steel_sum = 0, 0
+        self.cable_project = ''
+        self.cable_project_list = []
+        self.cable_model = ''
+        self.cable_model_list = []
+        self.cable_model_YJLV22_26_35_3_95_g, self.cable_model_YJV22_26_35_1_300_g = 0, 0
+        self.cable_model_cable_duct = 0
+        self.cable_model_YJLV22_26_35_3_95_d, self.cable_model_YJV22_26_35_1_300_d = 0, 0
 
-        self.foot_bolt_list = []
-        self.foot_bolt_unit_zjc1, self.foot_bolt_unit_zjc2, self.foot_bolt_unit_jjc1 = 0, 0, 0
-        self.foot_bolt_unit_jjc2, self.foot_bolt_unit_tw1, self.foot_bolt_unit_tw2 = 0, 0, 0
-        self.foot_bolt_unit_layer, self.foot_bolt_unit = 0, 0
-        self.foot_bolt_sum_zjc1, self.foot_bolt_sum_zjc2, self.foot_bolt_sum_jjc1 = 0, 0, 0
-        self.foot_bolt_sum_jjc2, self.foot_bolt_sum_tw1, self.foot_bolt_sum_tw2 = 0, 0, 0
-        self.foot_bolt_sum_layer, self.foot_bolt_sum = 0, 0
+    def cable_model(self, cable_project, cable_model):
+        self.cable_project = cable_project
+        self.cable_model = cable_model
 
-        self.used_numbers_base_type_list = []
-        self.used_numbers_base_zjc1, self.used_numbers_base_zjc2, self.used_numbers_base_jjc1 = 0, 0, 0
-        self.used_numbers_base_jjc2, self.used_numbers_base_tw1, self.used_numbers_base_tw2 = 0, 0, 0
-        self.used_numbers_base_layer = 0
+        if self.cable_project == "高压电缆":
+            if self.cable_model == "YJLV22-26/35-3×95":
+                self.cable_model_YJLV22_26_35_3_95_g = self.buried_cable_35_3
+            elif self.cable_model == "YJV22-26/35-1×300":
+                self.cable_model_YJV22_26_35_1_300_g = self.buried_cable_35_1
+        if self.cable_project == "电缆沟":
+            if self.cable_model == "电缆沟长度":
+                self.cable_model_cable_duct = 3.2
+        if self.cable_project == "电缆终端":
+            if self.cable_model == "YJLV22-26/35-3×95":
+                self.cable_model_YJLV22_26_35_3_95_d = self.tur_number * 2
+            elif self.cable_model == "YJV22-26/35-1×300":
+                self.cable_model_YJV22_26_35_1_300_d = self.line_loop_number * 2
 
-    def electrical_insulator_model(self, base_type, c25_unit, steel_unit, foot_bolt_unit):
-        self.used_numbers_base_type = base_type
-        self.c25_unit = c25_unit
-        self.steel_unit = steel_unit
-        self.foot_bolt_unit = foot_bolt_unit
-        if self.used_numbers_base_type == "ZJC1":
-            self.used_numbers_base_zjc1 = round_up(
-                (self.used_numbers_single_Z2_30 + self.used_numbers_double_SZ2_30) / 2, 0)
-            self.c25_unit_zjc1 = self.c25_unit
-            self.c25_sum_zjc1 = self.used_numbers_base_zjc1 * self.c25_unit_zjc1
-            self.steel_unit_zjc1 = self.steel_unit
-            self.steel_sum_zjc1 = self.used_numbers_base_zjc1 * self.steel_unit_zjc1 / 1000
-            self.foot_bolt_unit_zjc1 = self.foot_bolt_unit
-            self.foot_bolt_sum_zjc1 = self.used_numbers_base_zjc1 * self.foot_bolt_unit_zjc1
+    def sum_cal_cable(self, cable_project_li, cable_li):
+        self.cable_project_list = cable_project_li
+        self.cable_model_list = cable_li
 
-        if self.used_numbers_base_type == "ZJC2":
-            self.used_numbers_base_zjc2 = round_up(
-                (self.used_numbers_single_ZK_42 + self.used_numbers_double_SZK_42) / 2, 0)
-            self.c25_unit_zjc2 = self.c25_unit
-            self.c25_sum_zjc2 = self.used_numbers_base_zjc2 * self.c25_unit_zjc2
-            self.steel_unit_zjc2 = self.steel_unit
-            self.steel_sum_zjc2 = self.used_numbers_base_zjc2 * self.steel_unit_zjc2 / 1000
-            self.foot_bolt_unit_zjc2 = self.foot_bolt_unit
-            self.foot_bolt_sum_zjc2 = self.used_numbers_base_zjc2 * self.foot_bolt_unit_zjc2
-
-        if self.used_numbers_base_type == "JJC1":
-            self.used_numbers_base_jjc1 = self.used_numbers_single_J2_24 + self.used_numbers_double_SJ2_24
-            self.c25_unit_jjc1 = self.c25_unit
-            self.c25_sum_jjc1 = self.used_numbers_base_jjc1 * self.c25_unit_jjc1
-            self.steel_unit_jjc1 = self.steel_unit
-            self.steel_sum_jjc1 = self.used_numbers_base_jjc1 * self.steel_unit_jjc1 / 1000
-            self.foot_bolt_unit_jjc1 = self.foot_bolt_unit
-            self.foot_bolt_sum_jjc1 = self.used_numbers_base_jjc1 * self.foot_bolt_unit_jjc1
-
-        if self.used_numbers_base_type == "JJC2":
-            self.used_numbers_base_jjc2 = self.used_numbers_single_J4_24 + self.used_numbers_single_FS_18 \
-                                          + self.used_numbers_double_SJ4_24
-            self.c25_unit_jjc2 = self.c25_unit
-            self.c25_sum_jjc2 = self.used_numbers_base_jjc2 * self.c25_unit_jjc2
-            self.steel_unit_jjc2 = self.steel_unit
-            self.steel_sum_jjc2 = self.used_numbers_base_jjc2 * self.steel_unit_jjc2 / 1000
-            self.foot_bolt_unit_jjc2 = self.foot_bolt_unit
-            self.foot_bolt_sum_jjc2 = self.used_numbers_base_jjc2 * self.foot_bolt_unit_jjc2
-
-        if self.used_numbers_base_type == "TW1":
-            self.used_numbers_base_tw1 = self.used_numbers_single_Z2_30 + self.used_numbers_double_SZ2_30 \
-                                         - self.used_numbers_base_zjc1
-            self.c25_unit_tw1 = self.c25_unit
-            self.c25_sum_tw1 = self.used_numbers_base_tw1 * self.c25_unit_tw1
-            self.steel_unit_tw1 = self.steel_unit
-            self.steel_sum_tw1 = self.used_numbers_base_tw1 * self.steel_unit_tw1 / 1000
-            self.foot_bolt_unit_tw1 = self.foot_bolt_unit
-            self.foot_bolt_sum_tw1 = self.used_numbers_base_tw1 * self.foot_bolt_unit_tw1
-
-        if self.used_numbers_base_type == "TW2":
-            self.used_numbers_base_tw2 = self.used_numbers_single_ZK_42 + self.used_numbers_double_SZK_42 \
-                                         - self.used_numbers_base_zjc2
-            self.c25_unit_tw2 = self.c25_unit
-            self.c25_sum_tw2 = self.used_numbers_base_tw2 * self.c25_unit_tw2
-            self.steel_unit_tw2 = self.steel_unit
-            self.steel_sum_tw2 = self.used_numbers_base_tw2 * self.steel_unit_tw2 / 1000
-            self.foot_bolt_unit_tw2 = self.foot_bolt_unit
-            self.foot_bolt_sum_tw2 = self.used_numbers_base_tw2 * self.foot_bolt_unit_tw2
-
-        if self.used_numbers_base_type == "基础垫层":
-            self.used_numbers_base_layer = self.sum_used_numbers
-            self.c25_unit_layer = self.c25_unit
-            self.c25_sum_layer = self.used_numbers_base_layer * self.c25_unit_layer
-            self.steel_unit_layer = self.steel_unit
-            self.steel_sum_layer = self.used_numbers_base_layer * self.steel_unit_layer / 1000
-            self.foot_bolt_unit_layer = self.foot_bolt_unit
-            self.foot_bolt_sum_layer = self.used_numbers_base_layer * self.foot_bolt_unit_layer
-
-    def sum_cal_tower_base(self, tower_base_li, c25_unit_li, steel_unit_li, foot_bolt_li):
-        self.c25_unit_list = c25_unit_li
-        self.used_numbers_base_type_list = tower_base_li
-        self.steel_unit_list = steel_unit_li
-        self.foot_bolt_list = foot_bolt_li
-        for i in range(0, len(self.c25_unit_list)):
-            TowerBase.electrical_insulator_model(self, self.used_numbers_base_type_list[i], self.c25_unit_list[i],
-                                                 self.steel_unit_list[i], foot_bolt_list[i])
-
-        self.c25_sum = round_up((self.c25_sum_zjc1 + self.c25_sum_zjc2 + self.c25_sum_jjc1 + self.c25_sum_jjc2 +
-                                 self.c25_sum_tw1 + self.c25_sum_tw2 + self.c25_sum_layer), 2)
-        self.steel_sum = round_up((self.steel_sum_zjc1 + self.steel_sum_zjc2 + self.steel_sum_jjc1 + self.steel_sum_jjc2
-                                   + self.steel_sum_tw1 + self.steel_sum_tw2 + self.steel_sum_layer), 2)
-
-        self.foot_bolt_sum = round_up((self.foot_bolt_sum_zjc1 + self.foot_bolt_sum_zjc2 + self.foot_bolt_sum_jjc1 +
-                                       self.foot_bolt_sum_jjc2), 2)
-        # print(self.tower_type, self.tower_type_high, self.used_numbers)
+        for i in range(0, len(self.cable_project_list)):
+            Cable.cable_model(self, self.cable_project_list[i], self.cable_model_list[i])
 
 
-tower_type_list = ['单回耐张塔', '单回耐张塔', '单回耐张塔', '单回直线塔', '单回直线塔', '双回耐张塔', '双回耐张塔', '双回直线塔', '双回直线塔', '铁塔电缆支架']
-tower_type_high_list = ['J2-24', 'J4-24', 'FS-18', 'Z2-30', 'ZK-42', 'SJ2-24', 'SJ4-24', 'SZ2-30', 'SZK-42', '角钢']
-tower_weight_list = [6.8, 8.5, 7, 5.5, 8.5, 12.5, 17, 6.5, 10, 0.5, ]
-tower_height_list = [32, 32, 27, 37, 49, 37, 37, 42, 54, 0]
-tower_foot_distance_list = [5.5, 5.5, 6, 5, 6, 7, 8, 6, 8, 0]
+cable_project_list = ['高压电缆', '高压电缆', '电缆沟', '电缆终端', '电缆终端']
+cable_model_list = ['YJLV22-26/35-3×95', 'YJV22-26/35-1×300', '电缆沟长度', 'YJLV22-26/35-3×95', 'YJV22-26/35-1×300']
 
-c25_unit_list = [12, 16, 42, 80, 8.8, 10.2, 2.4]
-steel_unit_list = [300, 500, 750, 900, 600, 800, 0]
-foot_bolt_list = [100, 180, 280, 360, 100, 180, 0]
+project02 = Cable(25.3, 23.6, 1.55, 3, 31, 5)
 
-tower_base_list = ['ZJC1', 'ZJC2', 'JJC1', 'JJC2', 'TW1', 'TW2', '基础垫层']
-# 需要改成字典形式
+project02.sum_cal_cable(cable_project_list, cable_model_list)
 
-project02 = TowerBase(25.3, 23.6, 1.55, 3, 31, 5)
-project02.sum_cal_tower_type(tower_type_list, tower_type_high_list, tower_weight_list, tower_height_list,
-                             tower_foot_distance_list)
-
-project02.sum_cal_tower_base(tower_base_list, c25_unit_list, steel_unit_list, foot_bolt_list)
-
-print(project02.foot_bolt_sum_zjc1, project02.foot_bolt_sum_zjc2, project02.foot_bolt_sum_jjc1,
-      project02.foot_bolt_sum_jjc2,
-      project02.foot_bolt_sum_tw1, project02.foot_bolt_sum_tw2,
-      project02.foot_bolt_sum_layer, project02.foot_bolt_sum)
+print(project02.cable_model_YJLV22_26_35_3_95_g, project02.cable_model_YJV22_26_35_1_300_g,
+      project02.cable_model_cable_duct,
+      project02.cable_model_YJLV22_26_35_3_95_d, project02.cable_model_YJV22_26_35_1_300_d)
--- autocrword/models/chapter_6/ElectricalCircuit.py
+++ autocrword/models/chapter_6/ElectricalCircuit.py
@@ -1,10 +1,10 @@
 class ElectricalCircuit:
-    def __init__(self, single_circuit, double_circuit, buried_cable_35_1, buried_cable_35_3, tur_number,
+    def __init__(self, single_circuit, double_circuit, buried_cable_35_3, buried_cable_35_1, tur_number,
                  line_loop_number):
         self.single_circuit = single_circuit
         self.double_circuit = double_circuit
-        self.buried_cable_35_1 = buried_cable_35_1
         self.buried_cable_35_3 = buried_cable_35_3
+        self.buried_cable_35_1 = buried_cable_35_1
         self.tur_number = tur_number
         self.line_loop_number = line_loop_number
 
@@ -12,3 +12,6 @@ class ElectricalCircuit:
         self.aluminium_cable_steel_reinforced_length = 0
         self.aluminium_cable_steel_reinforced_weight = 0
         self.aluminium_cable_steel_reinforced_weight_length = 0
+
+
+#需要消除参数顺序问题
