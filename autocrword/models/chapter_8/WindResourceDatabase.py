366b0a05e9856e3caf4f4028e1cc95903f1eb326
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-03-12 17:00:19 +0800

    base finished chapter 8 70%

--- autocrword/models/chapter_8/WindResourceDatabase.py
+++ autocrword/models/chapter_8/WindResourceDatabase.py
@@ -2,73 +2,77 @@ import pandas as pd
 import math, os
 from docxtpl import DocxTemplate
 import numpy as np
-from RoundUp import round_up,round_dict_numbers
+from RoundUp import round_up, round_dict_numbers
 
 
 class WindResourceDatabase:
 
     def __init__(self):
-        self.Fortificationintensity, self.Ultimateload = 0, 0
-        self.Basictype = ''
-        self.basic_earthwork_ratio, self.basic_stone_ratio = 0, 0
-        self.data = pd.DataFrame()
-        self.data_timesnumber = pd.DataFrame()
-        self.numbers = 0
-        self.data_timesnumber_np, self.data_np, self.data_number_concat_np = [], [], []
+        self.BasicType, self.FortificationIntensity, self.UltimateLoad = '', 0, 0
+        self.numbers, self.basic_earthwork_ratio, self.basic_stone_ratio = 0, 0, 0
+        self.earth_excavation_tur, self.stone_excavation_tur, self.earth_work_back_fill_tur = 0, 0, 0
+        self.data_wind_resource = pd.DataFrame()
+        self.DataTurbine = pd.DataFrame()
 
-    def extraction_data(self, Fortificationintensity, Basictype, Ultimateload):
-        self.Fortificationintensity = Fortificationintensity
-        self.Basictype = Basictype
-        self.Ultimateload = Ultimateload
-        col_name = ['Fortificationintensity', 'Bearingcapacity', 'Basictype', 'Ultimateload', 'FloorradiusR', 'R1',
-                    'R2', 'H1', 'H2', 'H3', 'Pilediameter', 'number', 'length', 'Singlepilelength', 'Area', 'Volume',
-                    'Cushion', 'M48PrestressedAnchor', 'C80secondarygrouting']
+    def extraction_data(self, fortification_intensity, basic_type, ultimate_load):
+        self.FortificationIntensity = fortification_intensity
+        self.BasicType = basic_type
+        self.UltimateLoad = ultimate_load
+        col_name = ['FortificationIntensity', 'BearingCapacity', 'BasicType', 'UltimateLoad', 'FloorRadiusR', 'R1',
+                    'R2', 'H1', 'H2', 'H3', 'PileDiameter', 'Number', 'Length', 'SinglePileLength', 'Area', 'Volume',
+                    'Cushion', 'M48PreStressedAnchor', 'C80SecondaryGrouting']
 
-        Data = pd.read_excel(
+        self.DataTurbine = pd.read_excel(
             r'C:\Users\Administrator\PycharmProjects\Odoo_addons_NB\autocrword\models\chapter_8\chapter8database.xlsx',
             header=1, sheet_name='风机基础数据', usecols=col_name)
-        self.data = Data.loc[Data['Fortificationintensity'] == self.Fortificationintensity].loc[
-            Data['Basictype'] == self.Basictype].loc[Data['Ultimateload'] == self.Ultimateload]
-        return self.data
+        self.data_wind_resource = \
+            self.DataTurbine.loc[self.DataTurbine['FortificationIntensity'] == self.FortificationIntensity].loc[
+                self.DataTurbine['BasicType'] == self.BasicType].loc[
+                self.DataTurbine['UltimateLoad'] == self.UltimateLoad]
+        return self.data_wind_resource
 
     def excavation_cal(self, basic_earthwork_ratio, basic_stone_ratio):
         self.basic_earthwork_ratio = basic_earthwork_ratio
         self.basic_stone_ratio = basic_stone_ratio
-        self.earth_excavation = math.pi * (self.data['FloorradiusR'] + 1.3) ** 2 * (
-                self.data['H1'] + self.data['H2'] + self.data['H3'] + 0.15) * self.basic_earthwork_ratio
-        self.stone_excavation = math.pi * (self.data['FloorradiusR'] + 1.3) ** 2 * (
-                self.data['H1'] + self.data['H2'] + self.data['H3'] + 0.15) * self.basic_stone_ratio
-        self.earthwork_backfill = self.earth_excavation + self.stone_excavation - self.data['Volume'] - self.data[
-            'Cushion']
+        self.earth_excavation_tur = math.pi * (self.data_wind_resource['FloorRadiusR'] + 1.3) ** 2 * (
+                self.data_wind_resource['H1'] + self.data_wind_resource['H2'] + self.data_wind_resource[
+            'H3'] + 0.15) * self.basic_earthwork_ratio
 
-        self.data['Earthexcavation_tur'] = self.earth_excavation
-        self.data['Stoneexcavation_tur'] = self.stone_excavation
-        self.data['Earthworkbackfill_tur'] = self.earthwork_backfill
-        self.data['Reinforcement'] = self.data['Volume'] * 0.1
-        return self.data
+        self.stone_excavation_tur = math.pi * (self.data_wind_resource['FloorRadiusR'] + 1.3) ** 2 * (
+                self.data_wind_resource['H1'] + self.data_wind_resource['H2'] + self.data_wind_resource[
+            'H3'] + 0.15) * self.basic_stone_ratio
+
+        self.earth_work_back_fill_tur = self.earth_excavation_tur + self.stone_excavation_tur - self.data_wind_resource[
+            'Volume'] - self.data_wind_resource['Cushion']
+
+        self.data_wind_resource['EarthExcavation_Turbine'] = self.earth_excavation_tur
+        self.data_wind_resource['StoneExcavation_Turbine'] = self.stone_excavation_tur
+        self.data_wind_resource['EarthWorkBackFill_Turbine'] = self.earth_work_back_fill_tur
+        self.data_wind_resource['Reinforcement'] = self.data_wind_resource['Volume'] * 0.1
+        return self.data_wind_resource
 
     def generate_dict(self, data, numbers_list):
-        self.data = data
+        self.data_wind_resource = data
         self.numbers_list = numbers_list
         dict_wind_resource = {
             'numbers_tur': int(self.numbers_list[0]),
-            '土方开挖_风机': self.data.at[self.data.index[0], 'Earthexcavation_tur'],
-            '石方开挖_风机': self.data.at[self.data.index[0], 'Stoneexcavation_tur'],
-            '土石方回填_风机': self.data.at[self.data.index[0], 'Earthworkbackfill_tur'],
-            'C40混凝土_风机': self.data.at[self.data.index[0], 'Volume'],
-            'C15混凝土_风机': self.data.at[self.data.index[0], 'Cushion'],
-            '钢筋_风机': self.data.at[self.data.index[0], 'Reinforcement'],
+            '土方开挖_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'EarthExcavation_Turbine'],
+            '石方开挖_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'StoneExcavation_Turbine'],
+            '土石方回填_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'EarthWorkBackFill_Turbine'],
+            'C40混凝土_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'Volume'],
+            'C15混凝土_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'Cushion'],
+            '钢筋_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'Reinforcement'],
             '基础防水_风机': 1,
             '沉降观测_风机': 4,
-            '预制桩长_风机': self.data.at[self.data.index[0], 'Singlepilelength'],
-            'M48预应力锚栓_风机': self.data.at[self.data.index[0], 'M48PrestressedAnchor'],
-            'C80二次灌浆_风机': self.data.at[self.data.index[0], 'C80secondarygrouting'],
+            '预制桩长_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'SinglePileLength'],
+            'M48预应力锚栓_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'M48PreStressedAnchor'],
+            'C80二次灌浆_风机': self.data_wind_resource.at[self.data_wind_resource.index[0], 'C80SecondaryGrouting'],
         }
         return dict_wind_resource
 
 
 project01 = WindResourceDatabase()
-data = project01.extraction_data(Basictype='扩展基础', Ultimateload=70000, Fortificationintensity=7)
+data = project01.extraction_data(basic_type='扩展基础', ultimate_load=70000, fortification_intensity=7)
 numbers_list = [15]
 data_cal = project01.excavation_cal(0.8, 0.2)
 dict_wind_resource = project01.generate_dict(data_cal, numbers_list)
