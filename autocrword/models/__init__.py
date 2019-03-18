dd1bff37d59e0d5f0f8ccda93ad90640cb0fae6e
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-03-18 10:31:27 +0800

    server 10%

--- autocrword/models/__init__.py
+++ autocrword/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from . import autoelectrical,autoproject, autowind, autocivil, autoeconomic, autoelectrical
+from . import autoelectrical,autoproject, autowind, autoeconomic
 # from chapter_8.MainConstructionQuantitySummarySheet import *
